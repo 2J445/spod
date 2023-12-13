dd7ae2ad23a148bc4c8d65069b30d1e75fe32749
EC2 Default User <ec2-user@ip-172-31-20-70.ap-northeast-1.compute.internal>
2023-12-13 03:30:07 +0000

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
             <div class="container">
                 <div class="spod-logo">
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        <img src="{{ asset('image/logo.png')}}">
+                        <img src="{{ asset('image/logo.png')}}">TEST
                     </a>
                 </div>
                 <div class="search-form">
